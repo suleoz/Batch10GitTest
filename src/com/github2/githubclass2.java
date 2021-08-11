2779ca7fc132700873b46a88d595a016223dcb4e
suleoz <suleozdemir107@gmail.com>
2021-08-11 19:46:25 -0400

    the second commit

---
+++ src/com/github2/githubclass2.java
@@ -0,0 +1,18 @@
+package com.github2;
+
+public class githubclass2 {
+	public static void main (String[]args) {
+		
+		
+		
+		System.out.println("=this is the second commit file=====");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

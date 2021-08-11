e84dcab065c89e7fda76853be730b7bcd6834686
suleoz <suleozdemir107@gmail.com>
2021-08-11 19:36:14 -0400

    first commit of project

---
+++ src/com/github2/github2class1.java
@@ -0,0 +1,24 @@
+package com.github2;
+
+public class github2class1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		System.out.println("==========this is the first commit file =================");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
